a4d46545ca42f6e91237b907db2bce7953ddf883
Mathias Tranzer <mtranzer@dawan.fr>
2025-05-20 11:18:56 +0200

    Ajout liste dans component Admin

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,5 +1,11 @@
 export default function Admin() {
   return (
-    <div>Admin</div>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
   )
 }
